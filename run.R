419b6231becc90d5541f6c5963e4d95ce49e76c8
Juan F. Vesga <juanovesga@gmail.com>
2025-10-28 11:46:48 +0000

    updated Run script

--- run.R
+++ run.R
@@ -40,9 +40,19 @@ if (length(not_installed)) {
       "04-data-school-uk.R",
       "05-data-comix-short.R",
       "06-data-covid.R",
-      "07-generate-input-parameters-short.R",
-      "08-generate-priors.R"
-
+      "07-generate-input-parameters.R",
+      "08-generate-priors.R",
+      "09-run-mcmc.R",
+      "10-mcmc-dx.R",
+      "11-run-mcmc-fits.R",
+      "12-plot-mcmc-fits.R",
+      "13-model-selection.R",
+      "14-prepare-files-DIC.R",
+      "15-life-events.R",
+      "16-run-r0-estimates.R",
+      "17-run-emergence.R",
+      "18-plot-emergence.R",
+      "19-table-emergence.R"
     )
   )
   
@@ -55,32 +65,4 @@ if (length(not_installed)) {
 }
 
 
-
-# 
-# library(odin2)
-# library(dust2)
-# library(monty)
-# 
-# root<-here::here()
-# path<-here::here("src","model_2pars.R")
-# 
-# #odin2::odin_migrate(path = path, dest = path)
-# # Call model object
-# source(path)
-# 
-# # Call data 
-# source(here::here("src","utility_functions.R"))
-# source(here::here("scripts","load_data.R"))
-# source(here::here("scripts","setup.model.R"))
-# 
-# 
-# sys <- dust_system_create(noro_model(), pars = list(), dt = 1)
-# dust_system_set_state_initial(sys)
-# 
-# 
-# 
-# incidence<- c(2, 4, 1, 3, 2, 5)
-# cases    <- c(12,23,25,36,30,57)
-# 
-# dpois(cases/600, incidence/600, log = TRUE)
 
